ed135d661dec103c871d3f55221955902d54d68b
czhc <cedric@coingecko.com>
2024-05-26 17:20:33 +0800

    Add basic auth to /post

--- api/post.rs
+++ api/post.rs
@@ -1,8 +1,8 @@
 use vercel_runtime::{Body, Error, Request, Response, StatusCode};
-use octocrab::Octocrab;
+use octocrab::{Octocrab};
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
-use serde_json::from_slice;
+use serde_json::{json, from_slice};
 use std::env;
 
 #[derive(Deserialize)]
@@ -26,6 +26,7 @@ struct ErrorResponse {
 pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     dotenv::dotenv().ok();
 
+    let auth_token = env::var("BASIC_AUTH_TOKEN").expect("BASIC_AUTH_TOKEN must be set");
     let github_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN must be set");
     let octocrab = Octocrab::builder().personal_token(github_token).build().unwrap();
     let owner = env::var("GITHUB_USERNAME").expect("GITHUB_USERNAME must be set");
@@ -34,6 +35,23 @@ pub async fn handler(req: Request) -> Result<Response<Body>, Error> {
     let timestamp = Utc::now().timestamp().to_string();
     let filename = format!("collections/_bytes/{}.md", timestamp);
     let date = Utc::now().format("%Y-%m-%d %H:%M:%S %z").to_string();
+
+    let header = req.headers();
+
+    // Get the authorization header
+    if let Some(auth_header) = header.get("Authorization") {
+        if auth_header.to_str().unwrap_or("") != auth_token {
+            return Ok(Response::builder()
+                .status(StatusCode::UNAUTHORIZED)
+                .header("Content-Type", "application/json")
+                .body(Body::from(json!({"error": "Unauthorized"}).to_string()))?);
+        }
+    } else {
+        return Ok(Response::builder()
+            .status(StatusCode::UNAUTHORIZED)
+            .header("Content-Type", "application/json")
+            .body(Body::from(json!({"error": "Unauthorized"}).to_string()))?);
+    };
 
     let body = req.body();
     let post_data: PostData = match from_slice(body) {
